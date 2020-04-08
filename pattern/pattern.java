21d9f32fdefd679d93a443a20de5237ab03f7bc6
panditwalde <pandit.r@techchefs.in>
2020-04-08 20:32:21 +0530

    All pattern program

---
+++ pattern/pattern.java
@@ -0,0 +1,53 @@
+package com.pattern;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		 int i,j,k=0;
+		for( i=1;i<=10;i++)
+		{  
+			for( j=1;j<=11;j++)
+			{
+				/*if(i==1||i==3||j==4 ||j==2)*/
+				
+				/*if(i==j || (i+j==6))*/
+				/*if(i==1 || i==5 || j==1 || j==5)*/
+				/*if( i+j>6)*/
+				/*if(i==3 || j==3)*/
+				/*if(i==j || i+j==6)*/
+				/*if(i==1 || i==3 ||j==0 ||j==5 )A*/
+				/*if(i==1 ||i==3 || j==0 ||i==2 && j==5)P*/
+				/*if(i==1||i==3  || i==5 ||j==3 || j==4)*/
+				/*if((i+j)>=6)*/
+				 /*if(i==1 ||   i==4 ||i==2 && j==3||i==3 && j==2)Z*/
+			/*	if(i==j && i<=3 || i+j==6 && i<=3 ||j==3 && i>=3)Y*/
+				/*if(i+j>=6)*/
+				/*if(j>=6-i  || j>=4+i)
+*/				/*  if(i+j==6 && i<=3 || i==j && i<=3 )*/
+				System.out.print(" ");
+				/*if((j==1 || i==5)||(i==1||j==5))*/
+				/*if(j<=6-i||j>=6+i||j<=i+5 ||j>=17-i)*/
+				if(j<=6-i||j>=5+i ||j<=5-i+1||j>=17-i)
+			  {
+					  System.out.print("* ");
+				  }
+				else
+				{
+					System.out.print(" ");
+					 
+				}
+				 
+				 
+				  
+				  		  
+	   	   }
+			 System.out.println("");
+		
+		}
+				
+				
+		
+
+	}
+
+}
